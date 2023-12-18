cbeed1118e6fc442224e75433a2839fddabfa601
Brian <jeb.3332@gmail.com>
2023-12-18 21:03:03 +0800

    增加分頁內容

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,10 +32,10 @@ class BottomNavBar extends StatefulWidget {
 class _BottomNavBarState extends State<BottomNavBar> {
   int _selectIndex = 0;
 
-  static const List<Widget> _widget = <Widget>[
-    Text('asdf3'),
+  List<Widget> _widget = <Widget>[
+    MyListView(),
     Text('asdf2'),
-    Text('asdf1')
+    UserProfile(),
   ];
 
   void _onItemTapped(int index) {
@@ -66,5 +67,73 @@ class _BottomNavBarState extends State<BottomNavBar> {
     );
   }
 }
+
+/* list view */
+class MyListItem extends StatelessWidget {
+  final String title;
+
+  MyListItem({required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(title),
+      subtitle: Text('${title}_sub_description'),
+      // 其他列表項目的配置...
+    );
+  }
+}
+
+class MyListView extends StatelessWidget {
+  final List<String> items = ['Item 1', 'Item 2', 'Item 3', 'Item 4', '我想問卦'];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: items.length,
+      itemBuilder: (context, index) {
+        return MyListItem(title: items[index]); // 將MyListItem添加到ListView中
+      },
+    );
+  }
+}
+
+/* profile */
+class UserProfile extends StatelessWidget {
+  const UserProfile({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        Row(
+          children: [
+            Icon(Icons.icecream, color: Colors.blue, size: 32,),
+            Text('帳號:${'pypy'}', style: TextStyle(fontSize: 24) ),
+          ],
+        ),
+        Row(
+          children: [
+            Icon(Icons.icecream, color: Colors.blue, size: 32,),
+            Text('信箱:${'email@gmail.com'}', style: TextStyle(fontSize: 24), ),
+          ],
+        ),
+        Row(
+          children: [
+            Icon(Icons.numbers, color: Colors.blue, size: 32),
+            Text('發文數量${'10'}', style: TextStyle(fontSize: 24,color: Colors.blue),),
+          ],
+        ),
+        Row(
+          children: [
+            Icon(Icons.numbers, color: Colors.blue, size: 32),
+            Text('回文數量${'31'}', style: TextStyle(fontSize: 24,color: Colors.blue),),
+          ],
+        )
+      ],
+    );
+  }
+}
+
 
 
